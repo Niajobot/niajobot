4758b112710e482bdbad768be79881d2989fabf0
nicroz <nicolas.silvain@outlook.com>
2022-12-23 16:53:38 +0100

    Formatage du retour des suggestions

--- services/suggestion_noel_service.py
+++ services/suggestion_noel_service.py
@@ -15,13 +15,19 @@ class SuggestionNoelService():
         ref = db.reference("/")
         ref.child(streamer).child("suggestions_noel").push(value)
 
+    def formatMessage(self, suggestions):
+        def extractGameStrFormat(suggestion):
+            return '- {0}'.format(suggestion['game'])
+        return "Voici les jeux de noel\n{0}".format('\n'.join(map(extractGameStrFormat, suggestions)))
+
     def getSuggestionForStreamer(self, streamer):
+
         def extractValue(suggestion):
-            return suggestion[1]
+                return suggestion[1]
 
         ref = db.reference("/")
-        return list(map(extractValue, ref.child(streamer).child("suggestions_noel") \
+        return self.formatMessage(list(map(extractValue, ref.child(streamer).child("suggestions_noel") \
                         .order_by_child("status") \
                         .equal_to("SUBMITED") \
                         .get() \
-                        .items()))
+                        .items())))
